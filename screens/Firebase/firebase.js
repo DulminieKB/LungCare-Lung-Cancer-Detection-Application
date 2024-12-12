2d1c8c4f6f2ff3ccfd42b56756be53dc570cf385
DulminieKB <kdulminie@gmail.com>
2024-12-12 13:35:30 +0530

    first commit

--- screens/Firebase/firebase.js
+++ screens/Firebase/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCrnLUN7074E1ULGbRdWHGkcJ87AFtDba0",
-  authDomain: "lungcare-a2352.firebaseapp.com",
-  projectId: "lungcare-a2352",
-  storageBucket: "lungcare-a2352.appspot.com",
-  messagingSenderId: "869002522409",
-  appId: "1:869002522409:web:cb068fe43e679f347a1138",
-  measurementId: "G-Q18EY58355"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
